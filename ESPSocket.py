5a53af21848fa1078fb24890c613b84f0d9f4397
ap2108 <papercircuit555@gmail.com>
2024-03-19 19:33:42 +0530

    4 wheel bug fix

---
+++ ESPSocket.py
@@ -0,0 +1,28 @@
+import socket
+import struct
+
+class ESPSocket(socket.socket):
+    def __init__(self, ip, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.addr = (ip, port)
+        self.error = True
+
+    def pair(self):
+        try:
+            self.sock.sendto(b'\x77', self.addr)
+            data, client_addr = self.sock.recvfrom(1)
+            print("Paired")
+            if data==b'\x77' and client_addr==self.addr:
+                self.error = False
+        except Exception as e:
+            print(e)
+
+            self.error = True
+
+    def setWheelSpeed(self, w0, w1, w2, w3, forward):
+        try:
+            self.sock.sendto(struct.pack('BBBB?', w0, w1, w2, w3, forward), self.addr)
+        except Exception as e:
+            print(str(e))
+            self.error = True
+
